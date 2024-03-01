edef9273bc818a73ac151877a55c806a8ad0ed01
Rad Hoda <57790085+rodhoda@users.noreply.github.com>
2024-03-01 13:59:36 -0800

    Added the kfree() implementation
    Testing is still required

--- heap.s
+++ heap.s
@@ -38,5 +38,32 @@ _kalloc
 _kfree
 	;; Implement by yourself
 		MOV		pc, lr					; return from rfree( )
+  
+		MOV		R1, R0					; Move pointer address into register R1
+  		LDR		R2, =HEAP_TOP				; Load the HEAP_TOP value into R2
+    		LDR		R3, =HEAP_BOT				; Load the HEAP_BOT value into R3	
+
+		; If statement
+	    	CMP  		R1, R2					; If address is smaller than HEAP_TOP
+    		BLT  		_invalid_address			; Return Null
+   	 	CMP  		R1, R3					; If address is larger than HEAP_BOT
+    		BGT  		_invalid_address			; Return Null
+
+		 ; Compute the MCB address
+		LDR  		R4, =MCB_TOP     			; Load the top of the MCB
+    		SUB  		R1, R1, R2      			; Subtract HEAP_TOP from the pointer
+    		LSR  		R1, R1, #4       			; Divide the difference by 16 (logical shift right by 4 bits)
+    		ADD  		R1, R4, R1       			; Add the result to MCB_TOP to get the MCB address
+
+		; Call the _rfree function 
+		BL   		_rfree
+		CMP  		R1, #0           			; Check if MCB address passed into _rfree() returns 0
+		MOVEQ 		R1, #0          			; If above is true, address is assigned as null (zero)
+  									; If not true, we will pass the MCB adress
 		
+
+		_invalid_address
+		MOV  		R0, #0           			; Set the return value to NULL
+		
+
 		END
