fdd34b7875dff219d7066ba81282face38142577
Kayden Grant <74332434+kaydengrant@users.noreply.github.com>
2024-03-03 12:26:07 -0800

    fixed _heap_init not properly allocated mcb space

--- src/heap.s
+++ src/heap.s
@@ -19,19 +19,22 @@ INVALID		EQU		-1			; an invalid id
 ; Memory Control Block Initialization
 		EXPORT	_heap_init
 _heap_init
+		LDR		R0, =MCB_TOP
+		LDR 	R1, =MAX_SIZE
+		STR		R1, [R0]
+		
 		LDR 	R0, =MCB_TOP+0x4		; 0x20006804
-		LDR	R1, =0x20006C00
-		B	_heap_mcb_init
+		LDR		R1, =0x20006C00
+		MOV		R2, #0x0
 			
 _heap_mcb_init
 		CMP 	R0, R1
-		BEQ	_heap_init_done
-		LDR 	R2, [R0]			; R2 == mem[MCB index]
-		MOV	R2, #0x0			; set R2 to 0
-		ADD	R0, R0, #0x1			; increment index
-		LDR 	R2, [R0]			; R2 == mem[MCB index]
-		MOV	R2, #0x0			; set R2 to 0
-		ADD	R0, R0, #0x1			; increment index
+		BGE	_heap_init_done
+		
+		STR		R2, [R0]			; zero out mem[mcb index]
+		ADD		R0, R0, #1			; zero out mem[mcb index + 1]
+		STR		R2, [R0]
+		ADD		R0, R0, #2			; mcb index += 2
 		B 	_heap_mcb_init
 	
 _heap_init_done
@@ -42,18 +45,24 @@ _heap_init_done
 ; void* _k_alloc( int size )
 		EXPORT	_kalloc
 _kalloc
+		PUSH	{lr}
 		CMP	R0, #32
 		BGE	_ralloc_init
 		MOV	R0, #32
 
 _ralloc_init
-							; R0 == size
+									; R0 == size
 		LDR	R1, =MCB_TOP			; R1 == MCB_TOP == left
 		LDR	R2, =MCB_BOT			; R2 == MCB_BOT == right
 		LDR	R3, =MCB_ENT_SZ			; R3 == MCB_ENT_SZ
-		B	_ralloc
+		BL	_ralloc
 		
-_ralloc
+		POP		{lr}
+		MOV		pc, lr
+		
+_ralloc		
+		PUSH	{lr}
+		
 		SUB	R4, R2, R1			
 		ADD	R4, R4, R3			; R4 == entire
 		ASR	R5, R4, #1			; R5 == half
@@ -65,23 +74,25 @@ _ralloc
 		CMP	R0, R8
 		BGT	_no_alloc
 		
-		STMFD	sp!, {r0-r11,lr}		; save registers
+		PUSH	{r0-r8}		; save registers
 		SUB	R2, R6, R3
 		BL	_ralloc
-		LDMFD	sp!, {r0-r11,lr}		; resume registers
+		POP		{r0-r8}		; resume registers
 		
 		CMP	R12, #0x0
 		BEQ	_ralloc_right
+		
 		LDR	R9, [R6]			; R9 == mem[midpoint]
-		AND 	R9, R9, #0x01
+		AND R9, R9, #0x01
 		CMP	R9, #0
 		BEQ	_return_heap_addr
+		B	_ralloc_done
 		
 _ralloc_right
-		STMFD	sp!, {r0-r11,lr}		; save registers
+		PUSH	{r0-r8}		; save registers
 		MOV	R1, R6
 		BL	_ralloc
-		LDMFD	sp!, {r0-r11,lr}		; resume registers
+		POP		{r0-r8}		; resume registers
 		B 	_ralloc_done
 		
 _return_heap_addr
@@ -92,22 +103,30 @@ _no_alloc
 		LDR 	R9, [R1]			; R9 == mem[left]
 		AND 	R9, R9, #0x01
 		CMP	R9, #0
-		BEQ	_ralloc_done			; return null
+		BNE	_return_invalid			; return invalid
+		
 		LDR	R9, [R1]			; R9 == mem[left]
 		CMP	R9, R7
-		BLT	_ralloc_done			; return null
+		BLT	_return_invalid			; return invalid
+		
 		ORR	R9, R7, #0x01			; *(short *)&array[ m2a( left ) ] = act_entire_size | 0x01;
 		STR	R9, [R1]
-		LDR	R10, =MCB_TOP			; R10 == MCB_TOP
-		LDR	R11, =HEAP_TOP			; R11 == HEAP_TOP
-		SUB	R1, R1, R10			; left -= mcb_top
+		
+		LDR	R9, =MCB_TOP			; R9 == MCB_TOP
+		LDR	R10, =HEAP_TOP			; R10 == HEAP_TOP
+		SUB	R1, R1, R9			; left -= mcb_top
 		LSL	R1, R1, #4			; left *= 16
-		ADD	R11, R11, R1			; heap_top += left
-		MOV	R12, R11			; heap_addr = heap_top + ( left - mcb_top ) * 16
+		ADD	R10, R10, R1			; heap_top += left
+		MOV	R12, R10			; heap_addr = heap_top + ( left - mcb_top ) * 16
+		B	_ralloc_done
+		
+_return_invalid
+		MOV	R12, #0
 		B	_ralloc_done
 
 _ralloc_done
-		MOV		pc, lr
+		POP		{lr}
+		BX		LR
 		
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Kernel Memory De-allocation
@@ -116,5 +135,32 @@ _ralloc_done
 _kfree
 	;; Implement by yourself
 		MOV		pc, lr					; return from rfree( )
+  
+		MOV		R1, R0					; Move pointer address into register R1
+  		LDR		R2, =HEAP_TOP				; Load the HEAP_TOP value into R2
+    	LDR		R3, =HEAP_BOT				; Load the HEAP_BOT value into R3	
+
+		; If statement
+	    CMP  		R1, R2					; If address is smaller than HEAP_TOP
+    	BLT  		_invalid_address			; Return Null
+   	 	CMP  		R1, R3					; If address is larger than HEAP_BOT
+    	BGT  		_invalid_address			; Return Null
+
+		 ; Compute the MCB address
+		LDR  		R4, =MCB_TOP     			; Load the top of the MCB
+    	SUB  		R1, R1, R2      			; Subtract HEAP_TOP from the pointer
+    	LSR  		R1, R1, #4       			; Divide the difference by 16 (logical shift right by 4 bits)
+    	ADD  		R1, R4, R1       			; Add the result to MCB_TOP to get the MCB address
+
+		; Call the _rfree function 
+		BL   		_rfree
+		CMP  		R1, #0           			; Check if MCB address passed into _rfree() returns 0
+		MOVEQ 		R1, #0          			; If above is true, address is assigned as null (zero)
+												; If not true, we will pass the MCB adress
+												
+_rfree
+		
+_invalid_address
+		MOV  		R0, #0           			; Set the return value to NULL
 		
 		END
