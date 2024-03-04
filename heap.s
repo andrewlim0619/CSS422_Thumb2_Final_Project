1b7d8caa8fd22f54e30c031ff568bf94f94277de
Rad Hoda <57790085+rodhoda@users.noreply.github.com>
2024-03-04 12:59:51 -0800

    Update heap.s
    rfree() function WIP

--- heap.s
+++ heap.s
@@ -34,6 +34,42 @@ _kalloc
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Kernel Memory De-allocation
 ; void free( void *ptr )
+		
+_rfree
+
+		SUB 		R0, R0, 0x20000000
+  		LDR 		R1, =MCB_TOP
+  		SUB		R2, R0, R1		 		; mcb_offset = mcb_addr - mcb_top
+    
+		LDR 		R3, [R0]                 		; R3 = mcb_contents
+		ASR		R4, R3, #4		 		; R4 = mcb_chunk
+		LSL		R5, R3, #4		 		; R5 = mcb_size
+
+  		DIV 		R6, R2, R4	
+    		AND 		R7, R6, #1
+      		CMP 		R7, #0			 		; Line 146 in heap.c
+		BNE		odd_case		
+		
+		; Even Case
+		ADD 		R6, R0, R4
+  		LDR		R7, =MCB_BOT
+  		CMP		R6, R7					; Line 150 in heap.c
+		BGE		return_zero
+
+  		SUB		R6, R6, 0x20000000
+    		LDR		R7, [R6]				; R7 = mcb_buddy
+
+      		
+    	_odd_case:
+      		
+	return_zero:
+  		return 		R0, #0
+    		BX		LR					; Return from function
+  		
+    		
+  		
+		END
+
 		EXPORT	_kfree
 _kfree
 	;; Implement by yourself
@@ -62,7 +98,7 @@ _kfree
   									; If not true, we will pass the MCB adress
 		
 
-		_invalid_address
+		_invalid_address:
 		MOV  		R0, #0           			; Set the return value to NULL
 		
 
